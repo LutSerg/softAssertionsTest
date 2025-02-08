ca429ea00986c8ad57ebd6762b336008365ae26c
neriado <168669893+neriado@users.noreply.github.com>
2025-02-08 21:58:58 +0300

    deleted not used imports

--- src/test/java/ru/asteroster/softAssertionsTests/SoftAssertionTest.java
+++ src/test/java/ru/asteroster/softAssertionsTests/SoftAssertionTest.java
@@ -1,13 +1,11 @@
 package ru.asteroster.softAssertionsTests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionTest {

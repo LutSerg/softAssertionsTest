b28a500419e65ab1485d3a713504a60827c51181
neriado <168669893+neriado@users.noreply.github.com>
2025-02-08 21:57:39 +0300

    added test code

--- src/test/java/ru/asteroster/softAssertionsTests/SoftAssertionTest.java
+++ src/test/java/ru/asteroster/softAssertionsTests/SoftAssertionTest.java
@@ -1,4 +1,44 @@
 package ru.asteroster.softAssertionsTests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SoftAssertionTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+    }
+
+    @Test
+    void softAssertionTest() {
+
+        open("selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link").$("[type=button]").click();
+        $("#wiki-pages-box").shouldBe(appear)
+                .shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box")
+                .$("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
 }
